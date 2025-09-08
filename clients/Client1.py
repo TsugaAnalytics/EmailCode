22b78c05afdcecdb5be0fb1418055bfb8a07ea24
TsugaAnalytics <tsugaanalytics@gmail.com>
2025-09-07 20:50:42 -0500

    Delete clients directory

--- clients/Client1.py
+++
@@ -1,6 +0,0 @@
-def get_portfolio():
-    return {
-        "AAPL": 25,
-        "GOOGL": 30,
-        "Bonds": 45
-    }
--- clients/Client2.py
+++
@@ -1,6 +0,0 @@
-def get_portfolio():
-    return {
-        "MSFT": 40,
-        "AMZN": 35,
-        "Bonds": 25
-    }
