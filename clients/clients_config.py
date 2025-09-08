ca3f1dc70404e4fb022f9f858ce8c1a429ad05f9
TsugaAnalytics <tsugaanalytics@gmail.com>
2025-09-07 20:57:26 -0500

    Create clients_config.py

---
+++ clients/clients_config.py
@@ -0,0 +1,4 @@
+CLIENTS = [
+    "https://raw.githubusercontent.com/username/repo/main/clients/Client1/portfolio.py",
+    "https://raw.githubusercontent.com/username/repo/main/clients/Client2/portfolio.py"
+]
