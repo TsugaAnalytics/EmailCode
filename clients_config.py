50b9b2451047dffe22fd25f2997a9439df65944b
TsugaAnalytics <tsugaanalytics@gmail.com>
2025-09-07 22:08:47 -0500

    Delete clients_config.py

--- clients_config.py
+++
@@ -1,8 +0,0 @@
-clients = [
-    {
-        # Wilson Burton
-        "url": "https://raw.githubusercontent.com/TsugaAnalytics/email-testing2/refs/heads/main/clients/Client1.py",
-        "email": "wlsnburton@icloud.com"
-    }
-    # add more clients here
-]
