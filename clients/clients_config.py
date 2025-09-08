4815b3c877893bed5f281a177355d586fe20ef6d
TsugaAnalytics <tsugaanalytics@gmail.com>
2025-09-07 21:10:10 -0500

    Delete clients/clients_config.py

--- clients/clients_config.py
+++
@@ -1,4 +0,0 @@
-CLIENTS = [
-    "https://raw.githubusercontent.com/username/repo/main/clients/Client1/portfolio.py",
-    "https://raw.githubusercontent.com/username/repo/main/clients/Client2/portfolio.py"
-]
