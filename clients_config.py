0b7855b8a399bfd2ffddc44ec029661a3bf9962b
TsugaAnalytics <tsugaanalytics@gmail.com>
2025-09-07 21:15:36 -0500

    Update clients_config.py

--- clients_config.py
+++ clients_config.py
@@ -1,5 +1,6 @@
 clients = [
     {
+        # Wilson Burton
         "url": "https://raw.githubusercontent.com/TsugaAnalytics/email-testing2/refs/heads/main/clients/Client1.py",
         "email": "wlsnburton@icloud.com"
     }
