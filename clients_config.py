c4c942b9882062adbea12265dfd87b9bb3d2a778
TsugaAnalytics <tsugaanalytics@gmail.com>
2025-09-07 21:13:21 -0500

    Create clients_config.py

---
+++ clients_config.py
@@ -0,0 +1,7 @@
+clients = [
+    {
+        "url": "https://raw.githubusercontent.com/username/repo/main/clients/Client1.py",
+        "email": "wlsnburton@icloud.com"
+    }
+    # add more clients here
+]
